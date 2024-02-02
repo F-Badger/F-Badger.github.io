83bb52c8c13aca09df5421deb429378af547bf11
Lunatic1403 <127551365+Lunatic1403@users.noreply.github.com>
2024-02-02 01:21:27 +0000

    learn section beginning
    -placed some scripts in separate files
    -begun work on learn section header

---
+++ learn/learn_scripts.js
@@ -0,0 +1,30 @@
+let high_contrast = false
+window.addEventListener('message', 
+(event) => {
+    if (event.data == "toggleHighContrast") {
+        toggleHighContrast()
+        var headerObject = document.getElementById('header');
+        headerObject.contentWindow.postMessage("toggleHighContrast", "*")
+    }
+}
+)
+function toggleHighContrast() {
+    if (!high_contrast) {
+        document.body.classList.add("high-contrast")
+        document.body.classList.remove("default")
+        Array.from(document.getElementsByClassName("default-font-colour")).forEach(element => {
+            element.classList.remove("default-font-colour");
+            element.classList.add("high-contrast-font-colour")
+        })
+        high_contrast = true
+    }
+    else {
+        document.body.classList.add("default")
+        document.body.classList.remove("high-contrast")
+        Array.from(document.getElementsByClassName("high-contrast-font-colour")).forEach(element => {
+            element.classList.remove("high-contrast-font-colour");
+            element.classList.add("default-font-colour")
+        });
+        high_contrast = false
+    }
+}
