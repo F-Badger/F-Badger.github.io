ac3b9cae41684d6f258b68d7c7214c79427e5916
Lunatic1403 <127551365+Lunatic1403@users.noreply.github.com>
2024-02-09 20:59:17 +0000

    general improvements
    -moved files around and created new files to better organise project
    -renamed some files for consistency
    -moved JS for header to separate file
    -improved JS for header to reduce repeated code
    -fixed pseudo background for high contrast mode
    -changed background colour to be slightly lighter
    -added underline effect when desktop dropdown headers are being hovered over

---
+++ learn/header/header-scripts.js
@@ -0,0 +1,117 @@
+let high_contrast = false;
+let viewportHeight;
+const mainWindow = window.parent;
+const specMap = document.getElementById("spec-map");
+const title = document.getElementById("h1");
+const mobileNav = document.getElementById("mobile-nav");
+const hamburgerMenu = document.getElementById("hamburger");
+const pseudoBackground = document.getElementById("pseudo-background");
+
+window.addEventListener('message', 
+    (event) => {
+        if (event.data[0] == "toggleHighContrast") {
+            toggleHighContrast()
+        }
+        if (event.data[0] == "updateViewportHeight") {
+            updateViewportHeight(event.data[1])
+        }
+    }
+    )
+
+window.addEventListener('resize', handleResize);
+
+function toggleHighContrast() {
+    if (high_contrast) {
+        document.querySelectorAll(".high-contrast").forEach(element => {
+            element.classList.remove("high-contrast")
+            element.classList.add("default")
+            
+        }
+        )
+        high_contrast = false
+    }
+    else {
+        document.querySelectorAll(".default").forEach(element => {
+            element.classList.remove("default")
+            element.classList.add("high-contrast")
+            
+        }
+        )
+        high_contrast = true
+    }
+}
+
+function handleResize() {
+    requestViewportHeight()
+    if (document.querySelector(".big.topic").offsetHeight == 0) {
+        return false
+    }
+    var newHeight = document.querySelector(".big.topic").offsetHeight + "px"
+    var topic_headers = document.querySelectorAll(".small.topic")
+    topic_headers.forEach(function(topic) {
+        topic.style.height = newHeight
+    })
+    specMap.style.height = newHeight
+    title.style.height = newHeight
+    if (window.innerWidth > 680) {
+        if (mobileNav.classList.contains("display")) {
+            mobileNav.classList.remove("display")
+            updateiframeHeight("110px")
+        }
+        if (hamburgerMenu.classList.contains("selected")) {
+            hamburgerMenu.classList.remove("selected")
+        }
+        pseudoBackground.classList.remove("enabled")
+    }
+    resetMobileNav()
+    mainWindow.postMessage(["updateBodyHeight","initial"],"*")               
+}
+
+function resetMobileNav() {
+    Array.from(document.querySelectorAll(".mobile-dropdown.visible")).forEach(element => {
+            element.classList.remove("visible")
+        }
+        )
+    Array.from(document.querySelectorAll(".mobile-dropdown-toggle.selected")).forEach(element => {
+            element.classList.remove("selected")
+        }
+        )
+}  
+
+function updateiframeHeight(newHeight) {
+    mainWindow.postMessage(["updateiframeHeight",newHeight],"*")
+}
+function requestViewportHeight() {
+    mainWindow.postMessage(["requestViewportHeight"],"*")
+}
+function updateViewportHeight(newViewportHeight) {
+    viewportHeight = newViewportHeight
+}
+
+function hamburgerPress() {
+    resetMobileNav()
+    hamburgerMenu.classList.toggle("selected")
+    mobileNav.classList.toggle("display")
+    pseudoBackground.classList.toggle("enabled")
+    if (mobileNav.classList.contains("display")) {
+        updateiframeHeight("100vh")  
+    }
+    else {
+        setTimeout(() => updateiframeHeight("110px"),400)
+    }
+    if (!(hamburgerMenu.classList.contains("selected"))) {
+        mainWindow.postMessage(["updateBodyHeight","initial"],"*")
+    }         
+}
+
+function mobileDropdownTogglePress(dropdownNum) {
+    mobileNav.classList.toggle("noMinHeight")
+    document.querySelector(".mobile-dropdown-toggle."+dropdownNum).classList.toggle("selected")
+    document.querySelector(".mobile-dropdown."+dropdownNum).classList.toggle("visible")
+    var navHeight = mobileNav.offsetHeight
+    var newHeight = Math.max(navHeight,viewportHeight) + "px"
+    updateiframeHeight(newHeight)
+    mainWindow.postMessage(["updateBodyHeight",newHeight],"*")
+}
+
+handleResize();updateiframeHeight("110px")
