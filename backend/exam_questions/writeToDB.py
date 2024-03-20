33ffdd774197286389c8df193d6d1a23e62fff7e
Lunatic1403 <127551365+Lunatic1403@users.noreply.github.com>
2024-03-20 19:57:59 +0000

    getTopicIDs
    - separated functionality for retrieving topic IDs from topic names into separate function

--- backend/exam_questions/writeToDB.py
+++ backend/exam_questions/writeToDB.py
@@ -11,20 +11,9 @@ def writeToDatabase(year, level, paper, extendedResponse, questionNum, questionI
 
     cursor = database.cursor() #create MySQLCursor object to allow interaction with database
 
-    topicIDs = [] #initialise list of topicIDs
-
-    #query to retrieve topicIDs using topic names
-    query = "SELECT topic_id \
-                FROM tbl_topics WHERE \
-                topic_name = %s"
-    
-    #execute query for each topic to get the corresponding topicIDs, adding them to the list of topicsIDs
-    for topic in topics:
-        cursor.execute(query, (topic,)) #(topic,) converts topic into a tuple of one element, which is necessary for syntax
-        topicID = cursor.fetchone()[0]
-        topicIDs.append(topicID)
-
     questionID = f"{year}_{level}_{paper}_{questionNum}" #create questionID (primary key) by combining the year, level, paper and question number
+
+    topicIDs = getTopicIDs(topics, cursor) #get list of topic IDs
 
     #create SQL query to add data to tbl_examquestions
     #not needed to specify which fields to insert values into as all fields are being entered
@@ -52,3 +41,19 @@ def writeToDatabase(year, level, paper, extendedResponse, questionNum, questionI
     #close cursor and database connection
     cursor.close()
     database.close()
+
+def getTopicIDs(topics, cursor):
+    topicIDs = [] #initialise list of topicIDs
+
+    #query to retrieve topicIDs using topic names
+    query = "SELECT topic_id \
+                FROM tbl_topics WHERE \
+                topic_name = %s"
+    
+    #execute query for each topic to get the corresponding topicIDs, adding them to the list of topicsIDs
+    for topic in topics:
+        cursor.execute(query, (topic,)) #(topic,) converts topic into a tuple of one element, which is necessary for syntax
+        topicID = cursor.fetchone()[0]
+        topicIDs.append(topicID)
+
+    return topicIDs
