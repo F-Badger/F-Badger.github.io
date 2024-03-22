ebcfae5020dc1f2d122e02f27ac1e793e30d5375
Lunatic1403 <127551365+Lunatic1403@users.noreply.github.com>
2024-03-22 20:57:00 +0000

    minor form changes
    -removed some incorrect full stops from form questions
    -changed button text colour to white
    -added comments to scripts

--- footer/form-scripts.js
+++ footer/form-scripts.js
@@ -20,23 +20,33 @@ function toggleHighContrast() {
     document.querySelector("header").classList.toggle("high-contrast")
 }
 
+//get the radio buttons for the question about which sections they have used
 const sectionUsedRadios = document.getElementsByName("sectionsUsed")
+
+//get the divs containing questions for each section
 const learnSectionQuestions = document.getElementById("learnQuestions")
 const practiceSectionQuestions = document.getElementById("practiceQuestions")
 
 sectionUsedRadios.forEach(function(radio) {
-    radio.addEventListener("change", function() {
+
+    radio.addEventListener("change", function() { //add an event listener to each radio that listen for any changes
+
+        //if they have selected that they have only used the learn section
         if (this.value == "learn") {
-            learnSectionQuestions.classList.remove("hidden")
-            practiceSectionQuestions.classList.add("hidden")
+            learnSectionQuestions.classList.remove("hidden") //show the learn section questions
+            practiceSectionQuestions.classList.add("hidden") //hide the practice section questions
         }
+
+        //if they have selected that they have only used the practice section
         else if (this.value == "practice") {
-            learnSectionQuestions.classList.add("hidden")
-            practiceSectionQuestions.classList.remove("hidden")
+            learnSectionQuestions.classList.add("hidden") //hide the learn section questions
+            practiceSectionQuestions.classList.remove("hidden") //show the practice section questions
         }
+
+        //if they have selected that they have used both sections
         else {
-            learnSectionQuestions.classList.remove("hidden")
-            practiceSectionQuestions.classList.remove("hidden")
+            learnSectionQuestions.classList.remove("hidden") //show the learn section questions
+            practiceSectionQuestions.classList.remove("hidden") //show the practice section questions
         }
     })
 })
